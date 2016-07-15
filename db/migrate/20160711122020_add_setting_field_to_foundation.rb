9af2b09630dd88f83fddd29ab21f754d1453211b
Sandeep Chourey <sandeep.chourey@atharvasystem.com>
2016-07-15 12:08:58 +0530

    Add migration

--- db/migrate/20160711122020_add_setting_field_to_foundation.rb
+++
@@ -1,8 +0,0 @@
-class AddSettingFieldToFoundation < ActiveRecord::Migration
-  def change
-    add_column :foundations, :recurring, :boolean, :default => true
-    add_column :foundations, :open_to_the_public, :boolean, :default => true
-    add_column :foundations, :anyone_volunteer, :boolean, :default => true
-    add_column :foundations, :basic_info, :text
-  end
-end
