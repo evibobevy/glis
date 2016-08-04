efb67e0bbd6a8fb7fc1ac18c2264f7ae110e5427
Sandeep Chourey <sandeep.chourey@atharvasystem.com>
2016-08-04 18:20:38 +0530

    Worked on mobile calendar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   skip_before_action :protect_from_forgery
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+  # helper_method :mobile_device?
 
   private
 
@@ -12,4 +13,9 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
   end
 
+  # def mobile_device?
+  #   p "======================="
+  #   p request.user_agent
+  #   (request.user_agent =~ /Mobile/) != -1
+  # end
 end
