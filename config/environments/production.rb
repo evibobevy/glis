3896986f56f1c4bc69917356883c9f5ea9582572
Jesal Rathod <jesal.rathod@atharvasystem.com>
2016-08-09 17:25:24 +0530

    Added mail settings for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,24 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.smtp_settings = {
+
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: "demo9392@gmail.com",
+      password: "Demo@9392"
+  }
+
+  config.action_mailer.default_url_options = { :host => 'http://glis.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_deliveries = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
