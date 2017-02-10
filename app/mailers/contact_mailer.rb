08ec358009555f662fe30d9273a9e9fa275d166f
Ats Developer <ats9392.developer@gmail.com>
2017-02-10 13:11:36 +0530

    fixed responsive design issues

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ActionMailer::Base
   default from: "from@example.com"
   def contact_request(contact)
     @contact = contact
-    mail(to: 'demo9392@gmail.com', subject: 'New Contact Submitted')
+    mail(to: 'info@glis.org', subject: 'New Contact Submitted')
   end
   def contact_notification_mail(contact)
     @contact = contact
